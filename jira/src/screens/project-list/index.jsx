5141160fa2c1f60b7987fde352213969c7b9c4ab
郭云亮 <1060950677@qq.com>
2021-04-29 14:06:01 +0800

    feat: 添加useMount和useDebounce两个hook

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
@@ -14,24 +14,25 @@ export const ProjectListScreen = () => {
     name: "",
     personId: ""
   })
+  const debouncedParam = useDebounce(param, 500);
 
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
--- jira/src/utils/index.js
+++ jira/src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 // 判断当 value 等于 0 时
 export const isFalsy = (value) => value === 0 ? false : !value;
 
@@ -14,4 +16,49 @@ export const cleanObject = (object) => {
     }
   })
   return result;
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if(timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function() {
+//       func(...param);
+//     }, delay)
+//   }
+// }
+// const log = debounce(() => console.log('call'), 5000);
+// log();
+// log();
+// log();
+/**
+ * debounce 原理讲解
+ * 0s ---------> 1s ----------> 2s ----------> ...
+ * 一定要理解，这三个函数都是同步操作，所以它们都是在 0 ~ 1s 这个时间内瞬间完成的
+ * log() #1 // timeout#1
+ * log() #2 // 发现 timeout#1! 取消之，然后设置 timeout#2
+ * log() #3 // 发现 timeout#2! 取消之，然后设置 timeout#3
+ *          // 所以，log() #3 结束后，就只剩 timeout#3 独自等待了
+ */
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+
+  useEffect(() => {
+    // 每次在 value 变化之后，设置一个定时器
+    const timeout = setTimeout(() => setDebounceValue(value), delay)
+
+    // 在上一次 useEffect 运行完之后，再来执行这个回调函数（负责清理上一次的回调函数）
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debounceValue;
 }
