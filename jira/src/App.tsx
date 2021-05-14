5dd411f582fcf749da53771ed2931cf9b2b75784
郭云亮 <1060950677@qq.com>
2021-05-14 17:48:36 +0800

    feat:添加注册登录系统

--- jira/src/App.tsx
+++ jira/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import { ProjectListScreen } from "screens/project-list";
-import { LoginScreen } from 'screens/login';
+import {useAuth} from "./context/auth-context";
+import {AuthenticatedApp} from "./authenticated-app";
+import {UnauthenticatedApp} from "./unauthenticated-app";
+
 // import { TsReactTest } from "usetest/try-use-array"
+// import { ProjectListScreen } from "screens/project-list"
 
 function App() {
+  const {user} = useAuth()
   return (
     <div className="App">
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
       {/* <ProjectListScreen /> */}
       {/* <TsReactTest /> */}
-      <LoginScreen />
     </div>
   );
 }
