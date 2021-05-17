80e1d827554d4032def50e4b2d66c230785b6fce
郭云亮 <1060950677@qq.com>
2021-05-17 09:55:42 +0800

    feat:添加登录状态维持

--- jira/src/screens/project-list/index.tsx
+++ jira/src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { cleanObject, useDebounce, useMount } from "utils";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
+import {useHttp} from "../../utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -18,20 +19,24 @@ export const ProjectListScreen = () => {
 
   const [list, setList] = useState([])
 
+  const client = useHttp()
+
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
-      if(response.ok) {
-        setList(await response.json())
-      }
-    })
+    client('projects', {data: cleanObject(debouncedParam)}).then(setList)
+    // fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+    //   if(response.ok) {
+    //     setList(await response.json())
+    //   }
+    // })
   }, [debouncedParam])
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
-      if(response.ok) {
-        setUsers(await response.json())
-      }
-    })
+    client('users').then(setUsers )
+    // fetch(`${apiUrl}/users`).then(async response => {
+    //   if(response.ok) {
+    //     setUsers(await response.json())
+    //   }
+    // })
   })
 
   return <div>
